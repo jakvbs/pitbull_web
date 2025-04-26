68f4126517432327fdfecd9b1924d389c5bbba3b
jakvbs <j.sieczczynski@gmail.com>
2025-04-26 18:06:27 +0200

    Add header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
-    css: ['bulma', '~/assets/css/main.css'],
+    css: [
+        'bulma',
+        '@fortawesome/fontawesome-free/css/all.css',
+        '~/assets/css/main.css',
+    ],
     modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/fonts'],
 });
